2177abbc609b00ff8e187fbea54cf7dff965109e
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-21 04:53:26 +0300

    Update 3-dars.py

--- 3-dars.py
+++ 3-dars.py
@@ -7,8 +7,24 @@ Created on Thu Apr 21 03:50:58 2022
 
 #sonlar darsi (int)
 
-butun = int(input("Sonni kiriting: "))
-kvadrat = butun ** 2
-kub = butun ** 3
+#butun = int(input("Sonni kiriting: "))
+#kvadrat = butun ** 2
+#kub = butun ** 3
 
-print(f"Kiritilgan sonning kvadrati: {kvadrat}, Kiritilgan sonning kubi: {kub}")
+#print(f"Kiritilgan sonning kvadrati: {kvadrat}, Kiritilgan sonning kubi: {kub}")
+
+
+#yosh = int(input("Yoshingizni kiriting:"))
+
+#t_yil = 2022 - yosh
+
+#print(f"{t_yil}-yilda tug\'ilgan ekansiz ")
+
+a = int(input("Butun son kiriting:"))
+b = int(input("Butun son kiriting:"))
+
+yigindi = a + b
+ayirma = a - b
+kopaytma = a * b
+bolinma = a / b
+print(f" yig\'indi: {yigindi}, ayirma: {ayirma}, ko\'paytma: {kopaytma}, bo\'linma: {bolinma}") 
