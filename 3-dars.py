8c0991199648c32aa3e3728971a7e0ea3dd612b1
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-21 04:44:05 +0300

    Create 3-dars.py

---
+++ 3-dars.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 03:50:58 2022
+
+@author: Shaxriyor
+"""
+
+#sonlar darsi (int)
+
+butun = int(input("Sonni kiriting: "))
+kvadrat = butun ** 2
+kub = butun ** 3
+
+print(f"Kiritilgan sonning kvadrati: {kvadrat}, Kiritilgan sonning kubi: {kub}")
