a22d3a79e75df2a771938a2e767737f0200d5aaa
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-21 04:55:50 +0300

    Create dars_ozgaruvchi.py

---
+++ dars_ozgaruvchi.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 13:05:45 2022
+
+@author: Shaxriyor
+"""
+
+ism = "Damir"
+print(ism)
+xabar = "Joker"
+print(xabar)
+xabar = "Doira"
+print(xabar)
+radius = 5 
+pi = 3.14159
+yuza = pi*radius**2
+print("Radisu" , radius, "ga teng doira yuzi=", yuza)
+ism = "Ahmad"
+print("Mening ismim "+ism)
+
+
+
+
+
+
+ism = "Damir"
+Familiya ="Xolboyev"
+ism_familiya = f"{ism} {Familiya}"
+print(ism_familiya)
+
+
+
+fname = "James"
+lname = 'Bond'
+matn = f"Salom mening ismim {lname}. {fname} {lname}!"
+print(matn)
+
+
+
+tyil = 1997
+print(f"Siz {tyil}-yilda tug'ilgansiz")
+print(f"Sizni yoshingiz {2022-tyil}da")
