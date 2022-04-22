bb72d5d8954a568d93181eb3c811e2b5c5e129b5
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-22 09:34:56 +0300

    Create 4-dars.py

---
+++ 4-dars.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 09:16:30 2022
+
+4-dars Ro'yhatlar
+
+@author: Damir
+"""
+mevalar = ['olma', 'gilos', 'anjir', 'shaftoli', "o\'rik"]
+
+print("Birinchi meva nomi: ", mevalar[0])
+
+print("ikkinchi meva nomi: ", mevalar[1])
+
+print("To\'rtinchi meva nomi: ", mevalar[3])
+
+
+print("Uchinchi meva: ", mevalar[2].upper())
+
+print("Beshinchi meva: ", mevalar[4].title())
+
+
+
+narxlar = [15000, 18000, 65000, 45000, 35000]
+
+print(narxlar[1] + narxlar[3])
+
+sonlar = [-23, -32, 43, -34, 56, -76]
+
+print(sonlar[-3])
