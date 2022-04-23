f99d19c2d2ed25b809b80c3351d6f49dde66c795
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-23 08:13:20 +0300

    Create 5-dars.py

---
+++ 5-dars.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 07:35:44 2022
+Ro'yhatlar №2 
+ 
+@author: Damir
+"""
+
+#mevalar = ['olma', "o\'rik", 'behi', 'gilos']
+#mevalar.remove('behi')
+#print(mevalar)
+#mevalar.append("shaftoli")#mevalarga shaftoli qo'shamiz
+
+#del mevalar[1]
+
+
+
+#print(mevalar)
+
+
+#elementni sug'urib olish
+# bozorlik = ["kartoshka", "piyoz", "sabzi", "un", "guruch"]
+# print(bozorlik)
+# bozorlik.pop(1)
+# print(bozorlik)
+# bozorlik.pop()
+# print(bozorlik)
+
+# ismlar = ["Zafar", "Sardor", "Jahon", "Doston", "Xayrulla"]
+
+# xabar = "Salom " + ismlar[0] + " ishlaring yaxshimi\n"  +  ismlar[2] + " qo\'li gul usta"
+
+# print(xabar)
+
+sonlar = [12, -21, 3.12, 32.6, -12.8]
+
+yig = sonlar[0] + sonlar[1]
+print (sonlar)
+print(yig)
+sonlar[1] = 43
+
+sonlar.remove(-12.8)
+
+sonlar.insert(3,-65)
+
+print(sonlar)
+
+#moshinlar = []
+#print(moshinlar)
+
+#moshinlar.append("Gentra")
+#moshinlar.append('Tico')
+#moshinlar.append("Nexia")
+
+#moshinlar.insert(0, "Captiva")
+#print(moshinlar)
