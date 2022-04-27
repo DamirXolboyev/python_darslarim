f6a6a6ab7748cec41cc1f98bd08a4865f40d4420
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-27 13:04:17 +0300

    Create 6-dars.py

---
+++ 6-dars.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 12:56:18 2022
+
+Ro'yhatlarni tartiblash
+
+@author: Damir
+"""
+cars = ["bmw", "mers", "audi", "ford", "volvo", "gm", "tesla"]
+
+
+
+cars.sort(reverse = True)
+
+print(cars)
