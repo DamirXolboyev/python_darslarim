cbf1e8f83ed0a7999c345bd6e8ee99251ce0dd36
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-30 11:19:00 +0300

    Update 4-dars.py

--- 4-dars.py
+++ 4-dars.py
@@ -23,7 +23,15 @@ print("Beshinchi meva: ", mevalar[4].title())
 
 narxlar = [15000, 18000, 65000, 45000, 35000]
 
-print(narxlar[1] + narxlar[3])
+print(narxlar, narxlar[1] + narxlar[3])
+
+narxlar[0] = 17000
+
+narxlar[1] = 54000
+
+narxlar[3] = 30000
+
+print(narxlar)
 
 sonlar = [-23, -32, 43, -34, 56, -76]
 
