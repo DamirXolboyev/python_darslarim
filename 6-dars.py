31a14d17533e4c0d331c1132f7e2260c26dcdddf
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-30 10:43:12 +0300

    Update 6-dars.py

--- 6-dars.py
+++ 6-dars.py
@@ -6,10 +6,36 @@ Ro'yhatlarni tartiblash
 
 @author: Damir
 """
-cars = ["bmw", "mers", "audi", "ford", "volvo", "gm", "tesla"]
+# cars = ["bmw", "mers", "audi", "ford", "volvo", "gm", "tesla"]
 
 
 
-cars.sort(reverse = True)
+# cars.sort(reverse = True)
 
-print(cars)
+# print(cars)
+
+
+# mehmonlar = ["Odil", "Hamid", "Temur", "Avazbek", "Farrux"]
+
+# print("sorted() qaytargan ro\'yhat: ", sorted(mehmonlar))
+
+# print("Asl ro\'yhat o\'zgarmas qoldi: ", mehmonlar)
+
+# print(sorted(mehmonlar,reverse = True))
+
+# ages = [23, 12, 10, 9, 54, 19, 7]
+
+# ages.sort()
+
+# print(ages)
+
+# print(sorted(ages, reverse = True))
+
+
+fruits = ['pear', 'banana', 'apple', 'watermelon', 'lemon']
+
+fruits.reverse()
+
+print(fruits)
+
+print("Elementlar soni: ", len(fruits))
