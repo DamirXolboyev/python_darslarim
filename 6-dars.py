a16d6e3c45603bcd9dd5eb1852f00d90a6b3d3fd
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-04-30 11:18:43 +0300

    Update 6-dars.py

--- 6-dars.py
+++ 6-dars.py
@@ -8,6 +8,15 @@ Ro'yhatlarni tartiblash
 """
 # cars = ["bmw", "mers", "audi", "ford", "volvo", "gm", "tesla"]
 
+# my_cars = cars[0:3]
+
+# new_cars = cars[:4]
+
+# old_cars = cars[3:]
+
+# print(my_cars, new_cars, old_cars)
+
+
 
 
 # cars.sort(reverse = True)
@@ -32,10 +41,49 @@ Ro'yhatlarni tartiblash
 # print(sorted(ages, reverse = True))
 
 
-fruits = ['pear', 'banana', 'apple', 'watermelon', 'lemon']
+# fruits = ['pear', 'banana', 'apple', 'watermelon', 'lemon']
 
-fruits.reverse()
+# fruits.reverse()
 
-print(fruits)
+# print(fruits)
 
-print("Elementlar soni: ", len(fruits))
+# print("Elementlar soni: ", len(fruits))
+
+# sonlar = list(range(0,10))
+
+# print(sonlar)
+
+
+# juft_sonlar = list(range(0,20,2))
+
+# print("Juft sonlar:", juft_sonlar)
+
+# toq_sonlar = list(range(1,20,2))
+
+# print("toq sonlar",toq_sonlar)
+
+narxlar = [12000, 21000, 32000, 45000, 10000, 4920, 54300]
+
+arzon = min(narxlar)
+
+qimmat = max(narxlar)
+
+jami = sum(narxlar)
+
+print(narxlar)
+
+print("Eng arzon narx: ", arzon,\
+      "\n Eng qimmat narx: ", qimmat,\
+          "\n Jami: ", jami)
+
+sonlar1 = [1, 2, 3, 4, 5]
+
+sonlar2 = sonlar1[:]
+
+sonlar2.append(6)
+
+sonlar2.append(7)
+
+print("Bu sonlar1 ro\'yhati: ",sonlar1)
+
+print("Bu sonlar2 ro\'yhati: ", sonlar2)
