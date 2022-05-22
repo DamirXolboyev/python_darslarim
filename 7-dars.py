2c3d8e32b73d5d00d5cb31cdb6d42b9a74231b25
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-05-22 14:59:03 +0300

    Update 7-dars.py

--- 7-dars.py
+++ 7-dars.py
@@ -27,9 +27,38 @@ Created on Sun May 22 13:40:09 2022
     
 # print(sonlar)
 # print(sonlar_kvadrati)
+"""1-misol mustaqil ishlangan misol """
+# dostlar = []
+# print("5 ta eng yaqin do\'stingiz kim?")
+# for n in range(5):
+#     dostlar.append(input(f"\n{n+1}-ismni kiriting: "))
+#     print(dostlar)
+"""2-misol mustaqil ishlangan misol """
+# ismlar = ['Damir', 'Quvondiq', 'Javohir', 'Siroj', 'Abubakr', 'Humoyun']
 
-dostlar = []
-print("5 ta eng yaqin do\'stingiz kim?")
-for n in range(5):
-    dostlar.append(input(f"\n{n+1}-ismni kiriting: "))
-    print(dostlar)
+# for ism in ismlar:
+#     print(f"{ism} Diplom olganlaring bilan tabriklayman!!!")
+# a = len(ismlar)
+# print(f"Kod  {a} marta takrorlandi")
+"""3-misol mustaqil ishlangan misol """
+# sonlar = list(range(1,100,2))
+
+# for son in sonlar:
+#     print(f"{son} ning kubi {son**3}")
+"""4-misol mustaqil ishlangan misol """
+# kinolar = []
+
+# for kino in range(5):
+#     kinolar.append(input(f"\n sizning {kino+1}-sevimli kinoingiz: "))
+#     print(kinolar)
+"""5-misol mustaqil ishlangan misol """
+# a = int(input("bugun nechta odam bilan so\'rashdingiz: "))
+
+# ismlar = []
+
+# for ism in range(a):
+    
+#     ismlar.append(input(f"\n {ism+1}-salomlashgan odamingizni ismini kiriting: "))
+#     print(ismlar)
+# b = len(ismlar)
+# print(f"bugun siz {b} ta odam bilan salomlashgansiz")
