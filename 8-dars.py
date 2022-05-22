b3fe1ff9d54f55ae42b3ade98fb0413ca3c37edc
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-05-22 21:47:29 +0300

    Create 8-dars.py

---
+++ 8-dars.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 22 16:50:14 2022
+8-dars Shart operatorlari
+@author: Damir
+"""
+ism = 'damir'
+
+# print(ism!="Damir")
+
+# print(ism.title()=='Damir')
+
+# son = int(input("istalgan sonni kiriting: "))
+
+# if(son>0):
+#     print(son,"musbat son")
+
+# yosh = int(input("yoshingizni kiriting: "))
+
+# if(yosh<=7):
+#     print("sizga autobus tekin")
+    
+# else:
+#     print("chipta narxi 5000 so\'m")        
+# x, y = 25,50
+# print("x>y") if(x>y) else print("y>x")
+
+# ism = 'Damir'
+
+# print(ism.lower() == 'damir')
+
+# avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hundai']
+
+# for avto in avtolar:
+#     if(avto == 'bmw'):
+#         print(avto.upper())
+        
+#     else:
+#         print(avto.title())
+        
+# javob = float(input("12x6 nechiga teng?>>>"))
+
+# if(javob!=72):
+#     print("noto\'g\'ri javob")
+""" vazifalar №1"""
+# cars = ['mazda', 'toyota', 'hyundai', 'kia', 'gm']
+
+
+
+# for car in cars:
+#     if (car!='Gm'):
+#         print(car.upper())
+        
+#     else:
+#         print(car.title())
+"""vazifa №2"""
+# login = str(input("Loginingizni kiriting:"))
+
+# if (login == 'admin'):
+#     print("Xush kelibsiz, Admin. Foydalanuvchilarni ro\'yhatini ko\'rasizmi")
+    
+# else:
+#     print(f"Xush kelibsiz: {login}!")
+"""vazifa №3"""
+a = float(input('a sonni kiriting: '))
+b = float(input('b sonni kiriting: '))
+
+if(a==b):
+    print('sonlar teng')
+    
+else:
+    print('sonlar teng emas')
