9f5ea9ca0109b514bb853fda6eb035042cc4b518
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2022-05-23 05:42:35 +0300

    Create 9-dars.py

---
+++ 9-dars.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 23 05:17:59 2022
+9-dars Bir nechta shartlarni tekshirish
+@author: Damir
+"""
+son = int(input("son kiriting: "))
+
+if(son>0):
+    print("musbat son")
+elif(son<0):
+    print("manfiy son")
+else:
+    print("son 0 ga teng")
